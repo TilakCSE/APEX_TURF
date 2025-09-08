a0ecedb636f63af8694888c2735f8c7ef9779a86
Tilak <chauhantilak2005@gmail.com>
2025-09-08 13:43:34 +0530

    admin_update

---
+++ src/main/java/org/example/service/TurfService.java
@@ -0,0 +1,36 @@
+package org.example.service;
+
+import org.example.dao.TurfDao;
+import org.example.model.Turf;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TurfService {
+    private final TurfDao turfDao = new TurfDao();
+
+    public List<Turf> getAllTurfs() throws SQLException {
+        return turfDao.findAll();
+    }
+
+    public Turf getTurfById(long id) throws SQLException {
+        return turfDao.findById(id);
+    }
+
+    public void saveTurf(Turf turf, List<Long> sportIds) throws SQLException {
+        if (turf.getId() == null) {
+            // Create new turf
+            turfDao.create(turf, sportIds);
+        } else {
+            // Update existing turf
+            turfDao.update(turf, sportIds);
+        }
+    }
+
+    public void deleteTurf(long id) throws SQLException {
+        turfDao.delete(id);
+    }
+
+    public List<Long> getSportIdsForTurf(long turfId) throws SQLException {
+        return turfDao.findSportIdsForTurf(turfId);
+    }
+}
